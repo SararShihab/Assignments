f9014b28faf9ac0018737a0fa7b3709fb1becb1e
Md. Shihab Sarar <69312373+whitemafia@users.noreply.github.com>
2025-11-18 23:15:52 +0600

    .

--- 2-1/OOP/Exam Preparation/Chained Exception/ChainedException.java
+++ 2-1/OOP/Exam Preparation/Chained Exception/ChainedException.java
@@ -14,6 +14,7 @@ public class ChainedException {
     static void method() throws ArithmeticException {
         try {
             int a = 10 / 0;
+            System.out.println(a);
         } catch (ArithmeticException e) {
             ArithmeticException ae = new ArithmeticException("Arithmetic Exception");
             ae.initCause(e);
